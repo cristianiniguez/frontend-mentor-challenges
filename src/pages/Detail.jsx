1e14af99abf164845d1f92c5015970ffeeb55e3d
cristianiniguez <felizpc1@gmail.com>
2021-10-28 17:59:42 -0400

    detail page with spinner and fatal components

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,15 +1,18 @@
 import Back from '../components/Back';
+import Fatal from '../components/Fatal';
+import Spinner from '../components/Spinner';
 import Info from '../components/Info';
+
 import useCountry from '../hooks/useCountry';
 
 const Detail = (props) => {
-  const { country } = useCountry(props.match.params.code);
+  const { country, loading, error } = useCountry(props.match.params.code);
 
   return (
     <section>
       <div className='container'>
         <Back />
-        {country && <Info {...country} />}
+        {error ? <Fatal error={error} /> : loading ? <Spinner /> : country && <Info {...country} />}
       </div>
     </section>
   );
